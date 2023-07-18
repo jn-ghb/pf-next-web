7790fc8a835e9315f765c3794415524693e706fc
jnbz2999 <jnbz2999s@gmail.com>
2023-07-18 17:38:32 +0900

    テストコードの削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,7 +71,6 @@ export async function getStaticProps() {
   if (!process.env.NEXT_PUBLIC_API_BASE_URL) return;
   const response = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL);
   const data = await response.json();
-  console.log("test");
 
   return {
     props: {
